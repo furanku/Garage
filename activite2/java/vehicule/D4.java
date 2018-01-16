c608a8c4283ce20a293e94c6f59dc2bdb186ff05
furanku <frankensteffen@gmail.com>
2018-01-16 23:46:56 +0100

    set the mark of each car

--- activite2/java/vehicule/D4.java
+++ activite2/java/vehicule/D4.java
@@ -1,4 +1,7 @@
 package it.activite2.java.vehicule;
+
+import it.activite2.java.enumeration.Marque;
+
 /**
  * this class inherit from Vehicule
  * @see Vehicule
@@ -7,6 +10,15 @@ package it.activite2.java.vehicule;
  */
 
 public class D4 extends Vehicule {
+	/**
+	 * setting this mark,each D4 will always have TROEN as mark
+	 * @see Vehicule#getMarque()
+	 * @see Marque
+	 */
+	@Override
+	public Marque getMarque() {
 	
+		return Marque.TROEN;
+	}
 
 }
