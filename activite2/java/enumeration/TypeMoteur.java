19cc642a25aa8aa1c234528828435fa44c88a528
furanku <frankensteffen@gmail.com>
2018-01-19 01:10:11 +0100

    fixed grammatical errors

--- activite2/java/enumeration/TypeMoteur.java
+++ activite2/java/enumeration/TypeMoteur.java
@@ -1,24 +1,24 @@
 package it.activite2.java.enumeration;
 /**
  * The enumeration of the engine's type
- * @author furanku
+ * @author Furanku
  */
 
 public enum TypeMoteur {
 	/**
-	 * Diesel type of engine can be add to a car
+	 * Diesel type of engine can be added to a car
 	 */
 	DIESEL,
 	/**
-	 * ESSENCE type of engine can be add to a car
+	 * ESSENCE type of engine can be added to a car
 	 */
 	ESSENCE,
 	/**
-	 *HYBRIDE type of engine can be add to a car
+	 *HYBRIDE type of engine can be added to a car
 	 */
 	HYBRIDE,
 	/**
-	 * ELCTRIQUE type of engine can be add to a car
+	 * ELCTRIQUE type of engine can be added to a car
 	 */
 	ELECTRIQUE;
 
