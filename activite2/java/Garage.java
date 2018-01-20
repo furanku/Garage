bd1a96b82817ba06c28cb18bfc3d8333855c36c8
furanku <frankensteffen@gmail.com>
2018-01-20 22:40:22 +0100

    fixed the duplication problem

--- activite2/java/Garage.java
+++ activite2/java/Garage.java
@@ -1,6 +1,7 @@
 package it.activite2.java;
 
 import java.io.BufferedInputStream;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,43 +14,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.activite2.java.vehicule.Vehicule;
+
 /**
  * class garage to add and store each created car
  * @author Furanku
  */
-
 public class Garage {
 	private List<Vehicule> voitures ;
-	private String file = "file_garage/garage.txt";
-	ObjectOutputStream oos;
+	private String file = "garage.txt";
 	ObjectInputStream ois;
-	
+	ObjectOutputStream oos;
+
 	/**
 	 * constructor of the class 
 	 * the file where we will store each new car is created here 
 	 */
 	@SuppressWarnings("unchecked")
 	public Garage() {
-		
-		voitures = new ArrayList<>();//creation of a new ArrayList
-		
-		//catching the exceptions if the file doesn't exist
-		try {
-			ois = new ObjectInputStream(
-					new BufferedInputStream(
-						new FileInputStream(
-								new File(file))));
- 		  this.voitures = (List<Vehicule>) ois.readObject(); //reading the file if it exists 
- 			ois.close();
-		}catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch(FileNotFoundException e) {
-			System.err.print("Aucune voiture sauvegardée !\n"); //print this if the file doesn't exist 
-		}catch(IOException e) {
-			e.printStackTrace();
-		}	
+		 voitures = new ArrayList<>();//Instantiation of a new ArrayList
+			//catching the exceptions if the file doesn't exist
+			try {
+				ois = new ObjectInputStream(
+						new BufferedInputStream(
+							new FileInputStream(
+									new File(file))));
+				this.voitures = (List<Vehicule>) ois.readObject(); //reading the file if it exists 
+				ois.close();
+			}catch(ClassNotFoundException e) {
+				e.printStackTrace();
+			}catch(FileNotFoundException e) {
+				System.err.println("Aucune voiture sauvegardée !");//print this if the file doesn't exist 
+				try {
+					Thread.sleep(10);//as syserr is sometimes faster than sysout i make it sleeps for 10ms to always have a good output 
+				} catch (InterruptedException ie) {
+					ie.printStackTrace();
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
 	}
-	
 	/**
 	 * this method add each new car with all the options
 	 * in this method we will also save each car in the file 
@@ -57,39 +60,43 @@ public class Garage {
 	 * 			 	 this car 
 	 */
 	public void addVoiture(Vehicule voiture) {
-		this.voitures.add(voiture); //adding a new car
+			
+		 this.voitures.add(voiture);//adding a new car
 		try {
-			oos = new ObjectOutputStream(
-					new BufferedOutputStream(
-						new FileOutputStream(
-							new File(file))));
-			oos.writeObject(this.voitures); // writing in the file 
-			oos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+				oos = new ObjectOutputStream(
+						new BufferedOutputStream(
+							new FileOutputStream(
+								new File(file))));
+				
+				oos.writeObject(this.voitures); // writing in the file
+				oos.close();	
+			}catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
-	
+	 
 	/**
 	 * overriding the toString method of the Garage Class
-	 * final visualization
+	 * output visualization
 	 */
 	@Override
 	public String toString() {
-			
-		String affichage = "***************************\n";
+	   String affichage="";
+			   affichage = "***************************\n";
 			   affichage += "*  Garage OpenClassrooms  *\n";
 		       affichage += "***************************\n";
-		
-		 for (Vehicule vehicule : voitures) {
-			 affichage += "+ Voiture " 
-					   +vehicule.toString()
-					   +"d'une valeur totale de " 
-					   + vehicule.getPrix() 
-					   + " €\n";
-		 }
-		 return affichage;
+		       
+	   for (Vehicule vehicule : voitures) {
+			   affichage += "+ Voiture " 
+					   	 +vehicule.toString();
+	   }
+	   voitures = new ArrayList<>();/*re-initialization of the Arraylist to have the same result after the second try
+									  and avoid duplication of the created cars*/
+	   return affichage;				
+	
 	}
+
 }
+
